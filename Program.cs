617284ec5f45c158977221ad458cdfc3d8dff7be
Mantas <mantasrimkus2006@gmail.com>
2026-04-06 18:14:40 +0300

    First commit

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿
+using System.Text.Json;
+
+class User
+{
+    public int id { get; set; }
+    public string Vardas { get; set; }
+    public string Gmail { get; set; }
+}
+
+class Program
+{
+    static void Main()
+    {
+        User user = new User
+        {
+            id = 205604043,
+            Vardas = "Tomas",
+            Gmail = "Hotto@gmail.com"
+        };
+
+        string jsonString = JsonSerializer.Serialize(user, new JsonSerializerOptions
+        {
+            WriteIndented = true
+        });
+
+        File.WriteAllText("users.json", jsonString);
+
+        Console.WriteLine("Failas sukurtas!");
+        Console.WriteLine(jsonString);
+    }
+}
